fe5b3ed05816065b6572613be049a9943db1dc8c
hfsantiago <santiquiu@gmail.com>
2020-09-22 22:03:30 -0500

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = process.argv[2];
 
-request.get(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    let countWedge = 0;
-    const films = JSON.parse(body).results;
-    for (let i = 0; i < films.length; i++) {
-      const charList = films[i].characters;
-      for (let j = 0; j < charList.length; j++) {
-        if (charList[j] === 'https://swapi.co/api/people/18/' || charList[j] === 'http://swapi.co/api/people/18/') {
-          countWedge += 1;
-        }
-      }
-    }
-    console.log(countWedge);
-  }
+const URL = process.argv[2];
+
+request(URL, { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body.results.reduce((count, movie) => {
+    return movie.characters.find((character) => character.endsWith('/18/'))
+      ? count + 1
+      : count;
+  }, 0));
 });
